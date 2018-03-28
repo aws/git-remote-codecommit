9256c09bbdaaeb5c28be44783e36007df1848c21
Damian Johnson <atagar@torproject.org>
2018-03-28 14:24:03 -0700

    Include additional setup.py fields

--- setup.py
+++ setup.py
@@ -1,17 +1,29 @@
 #!/usr/bin/env python
 
+import os
 import setuptools
+
+def read(fname):
+  return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setuptools.setup(
   name = 'git-remote-codecommit',
-  version = '1.0',
+  packages = ['git-remote-codecommit'],
+  version = '0.1',
   descripion = 'Git remote prefix to simplify pushing to and pulling from CodeCommit.',
+  long_description = read('README.md'),
   author = 'Amazon Web Services',
+  url = 'https://github.com/awslabs/git-remote-codecommit',
   license = 'Apache License 2.0',
-  packages = ['git_remote_codecommit'],
   entry_points = {
     'console_scripts': [
       'git-remote-codecommit = git_remote_codecommit:main',
     ],
   },
+  classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Utilities',
+    'License :: OSI Approved :: Apache Software License',
+  ],
 )
