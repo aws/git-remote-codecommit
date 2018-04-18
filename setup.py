02ad015293a884f87d7ec1f97e763378c0694ed4
Damian Johnson <damiajoh@amazon.com>
2018-04-18 14:36:22 -0700

    Declare dependency on botocore
    Oops, had botocore during development so forgot to include it in our setup.py.
    Tested by doing a quick local install and ensuring it brought that in this
    time...
    
      % PYTHONPATH=/tmp/lib/python2.7/site-packages/ python setup.py install --prefix /tmp
      ...
      Processing dependencies for git-remote-codecommit==0.2
      Searching for botocore>=1.10.4
      Reading https://pypi.python.org/simple/botocore/
      Downloading https://files.pythonhosted.org/packages/22/66/9dcf61c1756a0de1ac86547a0fdb8c8641e345049cbf6f18c2ad6dab69d0/botocore-1.10.4-py2.py3-none-any.whl#sha256=77f2869b8c27afbab78b72ce6b74c75923421f364c7a0153ac1a38858c59cd91
      Best match: botocore 1.10.4
      Processing botocore-1.10.4-py2.py3-none-any.whl
      Installing botocore-1.10.4-py2.py3-none-any.whl to /tmp/lib/python2.7/site-packages
      ...

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ def read(fname):
 setuptools.setup(
   name = 'git-remote-codecommit',
   packages = ['git_remote_codecommit'],
-  version = '0.1',
+  version = '0.2',
   description = 'Git remote prefix to simplify pushing to and pulling from CodeCommit.',
   long_description = read('README.md'),
   author = 'Amazon Web Services',
   url = 'https://github.com/awslabs/git-remote-codecommit',
   license = 'Apache License 2.0',
+  install_requires = ['botocore >= 1.10.4'],
   entry_points = {
     'console_scripts': [
       'git-remote-codecommit = git_remote_codecommit:main',
