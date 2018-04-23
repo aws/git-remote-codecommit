867a397652d0e20ee4a069b2c861df8aa9accf85
Damian Johnson <damiajoh@amazon.com>
2018-04-23 13:37:18 -0700

    Version bump to 0.3
    Ok, now that this looks right in GitHub lets try publishing this to PyPI...

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 setuptools.setup(
   name = 'git-remote-codecommit',
   packages = ['git_remote_codecommit'],
-  version = '0.2',
+  version = '0.3',
   description = 'Git remote prefix to simplify pushing to and pulling from CodeCommit.',
   long_description = read('README.rst'),
   author = 'Amazon Web Services',
